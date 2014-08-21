97f9b5cbf8757cfc4664f26155695ed5df516af6
grlurton <grlurton@uw.edu>
2014-08-20 19:17:55 -0700

    Finalized validation set
    We get shp as a validation set that combines eHealth data and SDI data

--- code/make_validation_set.r
+++ code/make_validation_set.r
@@ -1,7 +1,13 @@
 setwd('J://Project/phc/nga/dhis')
 
+UniqueMatch <- function(data , indexVar){
+  aa <- ddply(data , .(indexVar) , function(x) nrow(x))
+  c(as.character(subset(aa , V1 == 1)[,1]))
+}
+
 OrgUnitsHierarchy <- read.csv('HierarchyData.csv')
 DHISLGA <- readShapePoly("LGAMap.shp")
+DHISFacilities <- read.csv('UnitsWithData.csv')
 
 #Select hierarchy of LGA Available in DHIS as of now
 AvailableHierarchy <- subset(OrgUnitsHierarchy , Level3 %in% DHISLGA$UnitName)
@@ -32,26 +38,32 @@ ehealthdata <- osmNigeria[osmNigeria$source == 'ehealthafrica.org' &
                             osmNigeria$amenity == 'hospital' &
                             !is.na(osmNigeria$source) & !is.na(osmNigeria$amenity),]
 
+######Refaire match for eHealth data
+
+DHISFacilities <- merge(DHISFacilities , AvailableHierarchy , by.x = 'x' , 
+                        by.y = 'Level5ID')
+colnames(DHISFacilities)[1] <- 'UnitID'
+
+
+
+
 ##Match eHealth with datasets from DHIS
-matchEhealthALL <- data.frame(ehealth = character() , data = character())
+matchEhealthALL <- data.frame(ehealth = character() , data = character() , unitId = character())
 for(i in seq(nrow(ehealthdata))){
-  out <- data.frame(ehealth = character() , data = character())
-  mm <- grep(ehealthdata$name[i] , MatchStrat1$match , 
+  out <- data.frame(ehealth = character() , data = character(), unitId = character())
+  mm <- grep(ehealthdata$name[i] , as.character(DHISFacilities$Level5) ,
              ignore.case = FALSE , value = TRUE)
-  if (length(mm) > 0){
-    out <- data.frame(ehealth = ehealthdata$name[i] , data = mm)
+  print(mm)
+  if (length(mm) == 1){
+    out <- data.frame(ehealth = ehealthdata$name[i] , data = mm , 
+                      unitId = DHISFacilities$UnitID[DHISFacilities$Level5 == mm])
     matchEhealthALL <- rbind(matchEhealthALL , out)
   }
 }
 
-rm(mm , i , out , LGA , LGAAlt , LGAData , LGADataAlt , NatFeatures)
-
-
-
-
-
 ## Only keep matches with facilities in Kano region 
 ## and that are matched on osm localities
+
 dfValidCorresp <- subset(matchEhealthALL , substr(data , 1 ,2) == 'kn')
 dfValidCorresp <- subset(dfValidCorresp ,
                          data %in% UniqueMatch(dfValidCorresp , dfValidCorresp$data))
@@ -61,6 +73,8 @@ dfValidCorresp <- subset(dfValidCorresp ,
 
 ValidationData <- ehealthdata[ehealthdata$name %in% dfValidCorresp$ehealth ,]
 
+ValidationData@data <- merge(ValidationData@data , dfValidCorresp , by.x = 'name' ,
+                             by.y = 'ehealth' , sort = F)
 
 
 ###################
@@ -83,7 +97,9 @@ SDIData6 <- merge(SDIData , AvailableHierarchy , by.x = 'UnitId' , by.y = 'Level
 SDIData <- rbind(SDIData5 , SDIData6)
 
 nn <- nrow(SDIData)
-SDIData <- data.frame(name = SDIData$facility_name , 
+SDIData <- data.frame(SDIname = SDIData$facility_name ,
+                      dhisName = SDIData$DHISFacility ,
+                      dhisId = SDIData$UnitId ,
                       lat = as.numeric(
                         unlist(strsplit(as.character(SDIData$coordinates) , ','))[2*seq(1:nn/2)-1]) ,
                       long = as.numeric(
@@ -93,7 +109,6 @@ SDIData <- data.frame(name = SDIData$facility_name ,
 SDIData <- subset(SDIData ,!is.na(long) & !is.na(lat) )
 
 SDILGAcrop <- data.frame(name = character() ,
-                         lat = numeric() ,
                          UnitName = character() ,
                          country = character())
 
@@ -114,10 +129,18 @@ for (LGA in unique(SDIData$lga)){
 }
 
 SDIDataShp <- subset(SDILGAcrop , !is.na(UnitName))
-SDIDataShp <- merge(SDIDataShp , SDIData , by = 'name')
+SDIDataShp <- merge(SDIDataShp , SDIData , by = 'SDIname')
 
 
 coordinates(SDIDataShp) = ~long+lat
+coordinates(SDIData)= ~long+lat
+
+
+par(mfrow = c(1,2))
+plot(DHISLGA)
+plot(SDIData , add = T , col = 'blue')
+plot(ehealthdata , add = T , col = 'red')
+title(main = 'Validation Set')
 
 
 plot(DHISLGA)
@@ -125,5 +148,19 @@ plot(SDIDataShp , add = T , col = 'blue')
 plot(ehealthdata , add = T , col = 'red')
 title(main = 'Validation Set')
 
+##merge the two validation data sets
 
-### Crop SDI sur les LGA from DHIS sur lesquels ils ont ete matches
+colnames(ValidationData@data)
+
+ValidationData@data <- subset(ValidationData@data , 
+                              select = c(unitId , name , data) )
+
+SDIDataShp@data <- subset(SDIDataShp@data , 
+                          select = c(dhisId.x , SDIname , dhisName.x) )
+
+
+colnames(SDIDataShp@data) <- colnames(ValidationData@data) <- c("UnitID" , "ValidationName" ,
+                                                            'DHISName')
+
+ValidationSet <- spRbind(SDIDataShp , ValidationData)
+plot(ValidationSet)
