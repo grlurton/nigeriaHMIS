7f091243ff1fdd86376a16d18114ffce6ace0163
grlurton <grlurton@uw.edu>
2014-08-21 17:13:53 -0700

    Combined steps matching
    Incremental match with different strategies

--- code/MatchGPSFinal.r
+++ code/MatchGPSFinal.r
@@ -8,12 +8,17 @@ library(ggplot2)
 
 setwd('J://Project/phc/nga/dhis')
 
+
+
 #orgUnits <- read.csv('ExtractOrgUnitsRaw.csv')
 OrgUnitsHierarchy <- read.csv('HierarchyData.csv')
 DHISLGA <- readShapePoly("LGAMap.shp")
+Locations <- read.csv('table_location.csv')
+Facilities <- Locations$location_id[Locations$location_level == 'Facility']
 
 #Select hierarchy of LGA Available in DHIS as of now
-AvailableHierarchy <- subset(OrgUnitsHierarchy , Level3 %in% DHISLGA$UnitName)
+AvailableHierarchy <- subset(OrgUnitsHierarchy , #Level3 %in% DHISLGA$UnitName &
+                               Level5ID %in% Facilities)
 
 ##Voir comment obtenir osm data directement
 osmData <- readShapePoints("C://Users/grlurton/Documents/NGAMap/NigeriaPointsEdit2.shp")
@@ -100,8 +105,6 @@ MatchStrat1 <- MatchStrat1[MatchStrat1$match %in%
                              UniqueMatch(MatchStrat1@data , MatchStrat1@data$match),
                            ]
 
-plot(MatchStrat1)
-
 ##Strategy 2 - match simply on all units in OSM 
 osmStrategy2 <- osmNigeria
 osmStrategy2@data$name <- paste(' ' , osmStrategy2@data$name , ' ' , sep = '')
@@ -110,8 +113,6 @@ MatchStrat2 <- MatchOver(AvailableHierarchy , osmStrategy2)
 MatchStrat2 <- MatchStrat2[MatchStrat2$match %in% 
                              UniqueMatch(MatchStrat2@data , MatchStrat2@data$match),
                            ]
-
-plot(MatchStrat2)
 
 ##Strategy 3 - simplify osm names by taking out natural feature indication
 osmStrategy3 <- osmNigeria
@@ -123,6 +124,100 @@ MatchStrat3 <- MatchOver(AvailableHierarchy , osmStrategy3)
 MatchStrat3 <- MatchStrat3[MatchStrat3$match %in% 
                              UniqueMatch(MatchStrat3@data , MatchStrat3@data$match),
                            ]
+
+##Strategy 4 - From Strategy 4 - if name has multiple match, in a < 10 km2 zone, put
+## in the middle
+osmStrategy4 <- osmNigeria
+NatFeatures <- 'River|Hill|Forest Reserve|Native Area'
+osmStrategy4@data$name <- gsub(NatFeatures , '' , osmStrategy4@data$name)
+osmStrategy4@data$name <- paste(' ' , osmStrategy4@data$name , ' ' , sep = '')
+osmStrategy4@data$name <- gsub('  ' , ' ' , osmStrategy4@data$name)
+MatchStrat4 <- MatchOver(AvailableHierarchy , osmStrategy4)
+
+Centroids <- data.frame(facility = character(), 
+                        place = character() ,
+                        centroidlat = numeric(),
+                        centroidlong = numeric() ,
+                        lat = numeric() ,
+                        long = numeric())
+for (place in unique(MatchStrat3$place)){
+  MatchesforPlace <- MatchStrat3[MatchStrat3$place == place , ]
+  if (nrow(MatchesforPlace@data) > 1){
+    for (facility in unique(MatchesforPlace$match)){
+      MatchFacPlace <- MatchStrat3[MatchesforPlace$match == facility , ]
+      if(nrow(MatchesforPlace@data) > 1){
+        nmatches <- nrow(MatchesforPlace@data)
+        centroid <- gCentroid(MatchesforPlace)
+        out <- data.frame(facility = rep(facility , nmatches), 
+                          place = rep(place , nmatches) ,
+                          centroidlat = rep(centroid@coords[,1] , nmatches) ,
+                          centroidlong = rep(centroid@coords[,2] , nmatches) ,
+                          lat = MatchesforPlace@coords[,1] ,
+                          long = MatchesforPlace@coords[,2]
+                          )
+        Centroids <- rbind(Centroids , out)
+        print('1 centroid de plus')
+      }
+    }
+  }
+}
+
+test <- subset(Centroids , place == ' Aba ')
+plot(test$lat , test$long , col = test$facility)
+points(test$centroidlat , test$centroidlong  , col = test$facility)
+
+
+
+##Strategy 5 - If multiple facilities have been found in a ward, attribute those in
+## the same wards to variations in the convex zone
+
+##Strategy Combine - combining first 3 strategies incrementally
+osmStrategyC1 <- osmNigeria[!is.na(osmNigeria$place) ,]
+osmStrategyC1@data$name <- paste(' ' , osmStrategyC1@data$name , ' ' , sep = '')
+osmStrategyC1@data$name <- gsub('  ' , ' ' , osmStrategyC1@data$name)
+MatchStratC1 <- MatchOver(AvailableHierarchy , osmStrategyC1)
+MatchStratC1 <- MatchStratC1[MatchStratC1$match %in% 
+                             UniqueMatch(MatchStratC1@data , MatchStratC1@data$match),
+                             ]
+MatchStratC1@data$MatchingStage <- 'Stage 1'
+
+
+##Step 2
+
+HierarchyStep2 <- subset(AvailableHierarchy , !(Level5 %in% MatchStratC1$match))
+osmStrategyC2 <- osmNigeria
+osmStrategyC2@data$name <- paste(' ' , osmStrategyC2@data$name , ' ' , sep = '')
+#osmStrategyC2 <- osmStrategyC2[!(osmStrategyC2@data$name %in% 
+#                                as.character(MatchStratC1$place)), ]
+osmStrategyC2@data$name <- gsub('  ' , ' ' , osmStrategyC2@data$name)
+MatchStratC2 <- MatchOver(HierarchyStep2 , osmStrategyC2)
+MatchStratC2 <- MatchStratC2[MatchStratC2$match %in% 
+                             UniqueMatch(MatchStratC2@data , MatchStratC2@data$match), 
+                             ]
+MatchStratC2@data$MatchingStage <- 'Stage 2'                           
+MatchStratC2 <- spRbind(MatchStratC2 , MatchStratC1)
+
+##Step 3
+
+HierarchyStep3 <- subset(AvailableHierarchy , !(Level5 %in% MatchStratC2$match))
+osmStrategyC3 <- osmNigeria
+osmStrategyC3@data$name <- gsub(NatFeatures , '' , osmStrategyC3@data$name)
+osmStrategyC3@data$name <- paste(' ' , osmStrategyC3@data$name , ' ' , sep = '')
+#osmStrategyC3 <- osmStrategyC3[!(osmStrategyC3@data$name %in% 
+#                                   as.character(MatchStratC2$place)), ]
+osmStrategyC3@data$name <- gsub('  ' , ' ' , osmStrategyC3@data$name)
+MatchStratC3 <- MatchOver(HierarchyStep3 , osmStrategyC3)
+MatchStratC3 <- MatchStratC3[MatchStratC3$match %in% 
+                               UniqueMatch(MatchStratC3@data , MatchStratC3@data$match), 
+                             ]
+MatchStratC3@data$MatchingStage <- 'Stage 3'                           
+MatchStratC3 <- spRbind(MatchStratC3 , MatchStratC2)
+                             
+                             
+                             
+rm(osmStrategy1 , osmStrategy2 , osmStrategy3 , osmStrategy4 , osmStrategyC1 ,
+   osmStrategy42 , osmStrategyC3)
+
 
 ##################################################
 ########## Validate Matching #####################
@@ -152,9 +247,9 @@ Validation <- function(TestedSet , ValidationSet){
   out
 }
 
-CompareSet1 <- Validation(MatchStrat1 , ValidationSet)
-CompareSet2 <- Validation(MatchStrat2 , ValidationSet)
-CompareSet3 <- Validation(MatchStrat3 , ValidationSet)
+CompareSet1 <- Validation(MatchStratC1 , ValidationSet)
+CompareSet2 <- Validation(MatchStratC2 , ValidationSet)
+CompareSet3 <- Validation(MatchStratC3 , ValidationSet)
 
 ValidationStatistics <- function(ValidationOutput){
   min5 <- sum(ValidationOutput$dist <5)/length(ValidationOutput$dist)
