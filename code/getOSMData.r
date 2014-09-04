4f6d47177acc3544d7ffd44c7694c5872d3589ec
grlurton <grlurton@uw.edu>
2014-09-03 19:38:48 -0700

    OSM extraction with data merging
    Gets data in OSM, takes the tags of nodes to merge to shapefile data

--- code/getOSMData.r
+++ code/getOSMData.r
@@ -2,20 +2,42 @@ library(osmar)
 library(maptools)
 
 setwd('J://Project/phc/nga/dhis')
-src <- osmsource_api()
 
 NigeriaShp <-  readShapePoly('LGAMap.shp')
 Nigeriabbox <- bbox(NigeriaShp)
-Nigeriabbox <- corner_bbox(Nigeriabbox[1,1], Nigeriabbox[2,1], Nigeriabbox[1,2], Nigeriabbox[2,2])
+#Nigeriabbox <- corner_bbox(Nigeriabbox[1,1], Nigeriabbox[2,1], Nigeriabbox[1,2], Nigeriabbox[2,2])
 
-NigeriaOSM <- get_osm(Nigeriabbox , source = src)
+url <- paste("http://www.overpass-api.de/api/xapi?" , 'node' , '[bbox=' ,
+             Nigeriabbox[1,1], ',' ,Nigeriabbox[2,1], ',' , Nigeriabbox[1,2], ',' , Nigeriabbox[2,2] ,
+             ']' , '[@meta]' ,
+             sep = '')
+             
+response <- getURL(url, .encoding = "UTF-8")       
+resp <- xmlParse(response)
+osmardata <- as_osmar(resp)
+shapefile <- as_sp(osmardata, what="points")
 
 
+library(reshape2)
 
-download.file("http://download.geofabrik.de/africa/nigeria-latest.osm.bz2","nigeria-latest.osm.bz2")
 
-## for now decompression should be done by hand in preferred software
+tt <- osmardata$nodes$tags
+tt$k <- as.character(tt$k)
+tt$k <- gsub(':' , '_'  ,tt$k)
+tt$k <- gsub('/' , '_'  ,tt$k)
+tt$k <- gsub(' ' , '_'  ,tt$k)
+tt$k <- gsub('__' , '_'  ,tt$k)
+length(unique(tt$k))
 
-src <- osmsource_osmosis(file = "nigeria-latest.osm/nigeria-latest.osm")
-Nigeria <- get_osm(Nigeriabbox, src)
-haha <- get_osm(complete_file(), source = osmsource_file("nigeria-latest.osm/nigeria-latest.osm"))
+reshTags <- dcast(tt , id ~ k)
+
+colnames(reshTags)[1] <- colnames(shapefile@data)[1] <- 'idtoMatch'
+
+shapefile@data <- merge(shapefile@data , reshTags , by = 'idtoMatch' ,
+                        sort = FALSE)
+
+table(shapefile@data$amenity)
+
+plot(NigeriaShp)
+plot(shapefile , col = factor(shapefile@data$amenity) , add = TRUE)
+
