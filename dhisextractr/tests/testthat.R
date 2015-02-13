d1684b8ac9acd11a0c40fd08ca7a6beb81c11121
Grégoire Lurton <grlurton@uw.edu>
2015-02-13 12:22:56 -0800

    taking out dhisextractr
    has its own repository now

--- dhisextractr/tests/testthat.R
+++
@@ -1,4 +0,0 @@
-library(testthat)
-library(dhisextractr)
-
-test_check("dhisextractr")
--- dhisextractr/tests/testthat/test_extraction.R
+++
@@ -1,5 +0,0 @@
-content('Testing basic data extraction functionnalities')
-
-
-extract_dhis_datasets('https://dhis2nigeria.org.ng/api/dataSets.xml' ,
-                      'grlurton' , 'Glurton29')
