2c31301b1aa457f9a98a51fe1fdee36fcd076b15
Grégoire Lurton <grlurton@uw.edu>
2015-02-17 12:03:35 -0800

    completing extraction

--- code/extract_dhis/data_extraction.r
+++ code/extract_dhis/data_extraction.r
@@ -1,7 +1,11 @@
-library("dhisextractr")
+library(dhisextractr)
 library(RCurl)
 library(XML)
 library(plyr)
+library(maptools)
+library(shapefiles)
+
+setwd('J://Project/dhis/nigeria/extracted_data')
 
 extracted_content <- extract_dhis_content(base_url = 'https://dhis2nigeria.org.ng/' , 
                                           userID = 'grlurton' , password = 'Glurton29')
@@ -16,15 +20,17 @@ org_units_data_sets <- extracted_content[[7]]
 
 time_deb <- Sys.time()
 
-extracted_kenya <- extract_all_data(base_url = 'https://dhis2nigeria.org.ng/' , userID = 'grlurton' , 
-                                    password = "Glurton29" ,
-                                    deb_period = '2009-01-01' , end_period = '2015-01-01' ,
-                                    org_units = org_units ,
-                                    data_sets = data_sets )
+extracted_nigeria <- extract_all_data(base_url = 'https://dhis2nigeria.org.ng/' , 
+                                      userID = 'grlurton' , 
+                                      password = "Glurton29" ,
+                                      deb_period = '2009-01-01' , 
+                                      end_period = '2015-01-01' ,
+                                      org_units = org_units ,
+                                      data_sets = data_sets )
 time_end <- Sys.time()
 
+### Writing Output ###
 
-setwd('J://')
 write.csv(data_sets , 'data_sets.csv')
 write.csv(data_elements , 'data_elements.csv')
 write.csv(data_categories , 'data_categories.csv')
@@ -34,6 +40,13 @@ write.csv(org_units_groups, 'org_units_groups.csv')
 write.csv(org_units_description, 'org_units_description.csv')
 
 
-write.csv(extracted_kenya , 'data_nigeria.csv')
+write.csv(extracted_nigeria , 'data_nigeria.csv')
 
 save.image('nigeria_initial_extraction.rdata')
+
+### Making shapefiles ###
+
+shapefiles <- extract_geolocalisation(org_units_description)
+
+write.shapefile(shapefiles[[1]], 'map_points', arcgis=T)
+write.shapefile(shapefiles[[2]], 'map_polygons', arcgis=T)
