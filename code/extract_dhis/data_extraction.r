5eeffd9d9ef90ccbbc23407e31a04d48dfa18f8b
Grégoire Lurton <grlurton@uw.edu>
2015-03-01 14:15:09 -0800

    saving content files before actual data extraction

--- code/extract_dhis/data_extraction.r
+++ code/extract_dhis/data_extraction.r
@@ -18,6 +18,14 @@ org_units_description <- extracted_content[[5]]
 org_units_groups <- extracted_content[[6]]
 org_units_data_sets <- extracted_content[[7]]
 
+write.csv(data_sets , 'data_sets.csv')
+write.csv(data_elements , 'data_elements.csv')
+write.csv(data_categories , 'data_categories.csv')
+write.csv(org_units , 'org_units.csv')
+write.csv(org_units_data_sets , 'org_units_data_sets.csv')
+write.csv(org_units_groups, 'org_units_groups.csv')
+write.csv(org_units_description, 'org_units_description.csv')
+
 time_deb <- Sys.time()
 
 extracted_nigeria <- extract_all_data(base_url = 'https://dhis2nigeria.org.ng/' , 
@@ -30,15 +38,6 @@ extracted_nigeria <- extract_all_data(base_url = 'https://dhis2nigeria.org.ng/' 
 time_end <- Sys.time()
 
 ### Writing Output ###
-
-write.csv(data_sets , 'data_sets.csv')
-write.csv(data_elements , 'data_elements.csv')
-write.csv(data_categories , 'data_categories.csv')
-write.csv(org_units , 'org_units.csv')
-write.csv(org_units_data_sets , 'org_units_data_sets.csv')
-write.csv(org_units_groups, 'org_units_groups.csv')
-write.csv(org_units_description, 'org_units_description.csv')
-
 
 write.csv(extracted_nigeria , 'data_nigeria.csv')
 
